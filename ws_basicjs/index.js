aa4d34c56485687566e0158701f5113fc394bf7c
William Green <up853829@xtec2018mbl21.uni.ds.port.ac.uk>
2018-10-01 13:19:58 +0100

    Fix 14

--- ws_basicjs/index.js
+++ ws_basicjs/index.js
@@ -3,12 +3,19 @@
 let remembered;
 
 function id() { return "UP853829"; }
+
 function fn() { return "Will"; }
+
 function sn() { return "Green"; }
+
 function add(num1, num2) { return num1 + num2; }
+
 function subtract(num1, num2) { return num1 - num2; }
+
 function checkObject(object = {checked: false}) { object.checked = true; }
+
 function checkObjectInside(object) { if(object.data != null) object.data.checked = true; }
+
 function arraySet(array, index, number) { if(index in array) array[index] = number; }
 
 function addAll(array) {
@@ -19,11 +26,19 @@ function addAll(array) {
 }
 
 function larger(num1, num2) { return Math.max(num1, num2); }
+
 function largest(array) { return Math.max(...array) }
+
 function compare(cArray1, cArray2) { return cArray1.toString() === cArray2.toString(); }
-function addToAll(array, adder) { return array.forEach((i) => { return array[i] + adder;}); }
+
+function addToAll(array, adder) { return array.map((index) => { return index + adder; }); } 
+
 function rememberThis(variable) { remembered = variable; }
+
 function nArray(n) { return Array.from({length: n}, (index) => index + 1); }
+
 function addAllOpt(array) { return Array.isArray(array)? array.reduce((head, tail) => head + tail, 0) : 0; }
+
 function divisors(array, divisor) { array.map(function(index) { return index / divisor; }); }
+
 function multiples(n, m) { return Array.from({length: n}, (index, m)  => 1*m, ); }
